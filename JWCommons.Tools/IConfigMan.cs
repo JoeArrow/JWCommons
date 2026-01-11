805bc1276cc9c4971f6b63631c7f1c424f2ac949
JoeArrow <joearrow1964@gmail.com>
2026-01-11 05:45:23 -0500

    Cleanup

--- JWCommons.Tools/IConfigMan.cs
+++ JWCommons.Tools/IConfigMan.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections;
+﻿#region Copyright © 2017 Joeware
+//
+// All rights reserved. Reproduction or transmission in whole or in part, in
+// any form or by any means, electronic, mechanical, or otherwise, is prohibited
+// without the prior written consent of the copyright owner.
+//
+#endregion
+
+using System.Collections;
 using System.Collections.Generic;
 
 namespace JWCommons.Tools
